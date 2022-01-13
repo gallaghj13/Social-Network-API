579a66df727b7ead30f229d4bc82f19a3f8e6fb9
John Gallagher <gallagherj0913@gmail.com>
2022-01-13 11:50:42 -0700

    Schema for thought model

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+       thoughtText: {
+           type: String,
+           required: true,
+           minLength: 1,
+           maxLength: 280,
+       },
+       createdAt: {
+           type: Date,
+           default: Date.now,
+        //    Use a getter method to format the timestamp on query
+       },
+       username: {
+           type: String,
+           required: true,
+       },
+       reactions: [Reaction],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Create virtual called reactionCount that retrieves the length of the thought's reactions array field on query
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+      return this.reactions.length;
+  });
+
+// Initialize Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
