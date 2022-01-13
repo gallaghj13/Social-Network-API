db41277964816c32fde5cba6ccd86aa1d06f3c46
John Gallagher <gallagherj0913@gmail.com>
2022-01-13 11:47:54 -0700

    just skeleton for thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,25 @@
+const Thought = require('../models/Thought');
+
+module.exports = {
+    getThoughts(req, res) {
+
+    },
+    getSingleThought(req, res) {
+
+    },
+    createThought(req, res) {
+
+    },
+    updateThought(req, res) {
+
+    },
+    deleteThought(req, res) {
+
+    },
+    createReaction(req, res) {
+
+    },
+    deleteReaction(req, res) {
+
+    },
+}
