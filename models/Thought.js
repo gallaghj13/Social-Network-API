57201ff81df24fd299001e432ee51154bf2e82da
John Gallagher <gallagherj0913@gmail.com>
2022-01-17 16:33:34 -0700

    updated with timestamp

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 const Reaction = require('./Reaction');
+const dateFormat = require('../utils/dateFormat')
 // Schema to create Thought model
 const thoughtSchema = new Schema(
     {
@@ -12,6 +13,7 @@ const thoughtSchema = new Schema(
        createdAt: {
            type: Date,
            default: Date.now,
+           get: timestamp => dateFormat(timestamp),
         //    Use a getter method to format the timestamp on query
        },
        username: {
