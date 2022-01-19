b14f03c2d880a2115e69584ff98a41eeb69254c1
John Gallagher <gallagherj0913@gmail.com>
2022-01-19 15:50:05 -0700

    debug

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 const Reaction = require('./Reaction');
-const dateFormat = require('../utils/dateFormat')
+const dateFormat = require('../utils/dateFormat');
 // Schema to create Thought model
 const thoughtSchema = new Schema(
     {
@@ -19,6 +19,7 @@ const thoughtSchema = new Schema(
        username: {
            type: String,
            required: true,
+           ref: 'User'
        },
        reactions: [Reaction],
     },
